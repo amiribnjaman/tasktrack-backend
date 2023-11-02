67e719115ba27fa6f12e54cae515de637ad1a443
amiribnjaman <amiribnjaman@gmail.com>
2023-11-02 20:01:30 +0600

    User controller implemented

---
+++ controller/userController.js
@@ -0,0 +1,61 @@
+const User = require("../model/user.model")
+const { v4: uuidv4} = require("uuid")
+
+
+const getAllUser = async (req, res) => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
+
+const getOneUser = async (req, res) => {
+    try{
+        const oneUser = await User.findOne({id: req.params.id})
+        res.status(200).json(oneUser)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+    
+}
+
+const createUser = async (req, res) => {
+    try{
+        const newUser = new User({
+            id: uuidv4(),
+            name: req.body.name,
+            age: +(req.body.age)
+        })
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch(error) {
+        res.status(500).send(error.message)
+    }
+}
+
+const updateUser = async (req, res) => {
+    try{
+        const user = await User.findOne({id: req.params.id})
+        user.name = req.body.name
+        user.age = +(req.body.age)
+        await user.save()
+        res.status(200).json(user)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try{
+        await User.deleteOne({id: req.params.id})
+        res.status(200).json({
+            message: 'User deleted'
+        })
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+}
+
+module.exports = { getAllUser, getOneUser, createUser, updateUser, deleteUser }
