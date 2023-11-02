66689e1001dfcf113d4d80427d60bf005edfd7ef
amiribnjaman <amiribnjaman@gmail.com>
2023-11-02 21:24:06 +0600

    task model schema designed

--- model/taskModel.js
+++ model/taskModel.js
@@ -1,7 +1,34 @@
 const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
-
+  id: {
+    type: String,
+    require: true,
+    },
+    creatorEmail: {
+        type: String,
+        require: true
+    },
+    title: {
+        type: String,
+        require: true,
+    },
+    completion: {
+        type: String,
+        require: true,
+    },
+    teamLeader: {
+        type: String,
+        require: true,
+    },
+    teamMemberNum: {
+        type: String,
+        require: true,
+    },
+    teamMembers: {
+        type: String | Array,
+        require: true,
+    }
 });
 
 module.exports = mongoose.model("Task", taskSchema);
